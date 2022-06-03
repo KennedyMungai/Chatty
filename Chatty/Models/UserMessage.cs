54c36513d6bc5604b40854a13549df76825d1e4a
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-03 22:14:27 +0300

    Added the required fields

--- Chatty/Models/UserMessage.cs
+++ Chatty/Models/UserMessage.cs
@@ -2,5 +2,8 @@ namespace Chatty.Models;
 
 public class UserMessage
 {
-    
+    public string? UserName { get; set; }
+    public string? Message { get; set; }
+    public bool CurrentUser { get; set; }
+    public DateTime DateSent { get; set; }
 }
