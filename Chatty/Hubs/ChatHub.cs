bf160acaf9c82fed3f0c080b8ebd1c78653006c9
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-03 21:34:15 +0300

    Added SendMessage Async Method

--- Chatty/Hubs/ChatHub.cs
+++ Chatty/Hubs/ChatHub.cs
@@ -4,5 +4,8 @@ namespace Chatty.Hubs;
 
 public class ChatHub : Hub
 {
-    
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
 }
